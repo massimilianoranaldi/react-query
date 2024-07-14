1332ba8c402a72768c91557fdd464d058ef8e683
DIR\massimiliano.ranaldi <massimiliano.ranaldi@accenture.com>
2024-07-14 10:54:30 +0200

    pulizia codice

--- src/components/Link.jsx
+++
@@ -1,9 +0,0 @@
-function Link({ children }) {
-  return (
-    <>
-      <a href="#">{children}</a>
-    </>
-  );
-}
-//children serve per consentire al componente di avere al suo interno un parametro
-export default Link;
