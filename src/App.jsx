1d78b9b60aae4df2d01d351b5195ae9bf6cc6d13
DIR\massimiliano.ranaldi <massimiliano.ranaldi@accenture.com>
2024-07-14 18:18:11 +0200

    eventi, passaggio props da padre a figlio, array contenuto in file js

--- src/App.jsx
+++ src/App.jsx
@@ -13,53 +13,85 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import citta from "./assets/cities";
+
 // sezione componenti
 
 import "./components/Card"; //importo libreria
 import Card from "./components/Card"; //creo oggetto
 
-function App() {
-  const [count, setCount] = useState(0);
+//nel caso viene dichiara fuori
+function handleConteggio2(conteggio, setConteggio) {
+  setConteggio((conteggio) => conteggio + 1);
+}
 
-  const cities = [
-    {
-      id: 1,
-      nome: "TOKYO",
-      urlImmagine:
-        "https://images.unsplash.com/photo-1513407030348-c983a97b98d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRva3lvfGVufDB8fDB8fHww",
-      descrizione: `
-ciao signori, 
-io sono tokio.
-coi chi siete ?...
-`,
-      visitato: true,
-    },
-    {
-      id: 2,
-      nome: "ROMA",
-      urlImmagine:
-        "https://images.unsplash.com/photo-1604580864964-0462f5d5b1a8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9tYXxlbnwwfHwwfHx8MA%3D%3D",
-      descrizione: `
-ciao signori, 
-io sono roma.
-coi chi siete ?...
-`,
-      visitato: false,
-    },
-  ];
+function App() {
+  const [count, setCount] = useState(0); //uestate è composto da due oggetti : una variabile e lafunzione che la modifica: lo stato precedente viene person e sovrascritto con quello attuale
+  const cities = [...citta];
+  //let conteggio = 0;
+
+  const [conteggio, setConteggio] = useState(0); //uestate è composto da due oggetti : una variabile e lafunzione che la modifica: lo stato
+  //dichiarata dentro la funzione principale
+  function handleConteggio() {
+    setConteggio((conteggio) => conteggio + 1);
+  }
+
+  //esempio di stati ad array
+  const [item, setItems] = useState([1, 2, 3]);
+  function aggiornaItem() {
+    const nuovoItem = 4;
+    console.log("ITEM : ", item);
+    setItems([...item, nuovoItem]);
+  }
+
+  //oppure un oggetto
+  const [user, setUser] = useState({ name: "Alice", age: "21" });
+  const updateUser = () => {
+    const newUser = { name: "pippo", age: "33" };
+    console.log("USER : ", user);
+    setUser(newUser);
+  };
+  //   const cities = [
+  //     {
+  //       id: 1,
+  //       nome: "TOKYO",
+  //       urlImmagine:
+  //         "https://images.unsplash.com/photo-1513407030348-c983a97b98d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRva3lvfGVufDB8fDB8fHww",
+  //       descrizione: `
+  // ciao signori,
+  // io sono tokio.
+  // coi chi siete ?...
+  // `,
+  //       visitato: true,
+  //     },
+  //     {
+  //       id: 2,
+  //       nome: "ROMA",
+  //       urlImmagine:
+  //         "https://images.unsplash.com/photo-1604580864964-0462f5d5b1a8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9tYXxlbnwwfHwwfHx8MA%3D%3D",
+  //       descrizione: `
+  // ciao signori,
+  // io sono roma.
+  // coi chi siete ?...
+  // `,
+  //       visitato: false,
+  //     },
+  //   ];
 
   return (
     <>
       <div className="grid grid-cols-4 gap-10">
-        {cities.map((city) => (
-          <Card
-            key={city.id}
-            titolo={city.nome}
-            urlImmagine={city.urlImmagine}
-            descrizione={city.descrizione}
-            visitato={city.visitato}
-          ></Card>
-        ))}
+        {cities
+          //.filter((city) => city.visitato)
+          .map((city) => (
+            <Card
+              key={city.id}
+              titolo={city.nome}
+              urlImmagine={city.urlImmagine}
+              descrizione={city.descrizione}
+              visitato={city.visitato}
+            ></Card>
+          ))}
         {/* 
         <Card
           titolo={cities[0].nome}
@@ -78,6 +110,24 @@ coi chi siete ?...
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {
+          // conteggio viene incrementato, ma poichè non è una variabile di stato, nella pagina non si vede il refresh e quindi react non la aggiorna. quindi devo far diventare conteggio una variabile di stato
+        }
+        {/* <button onClick={() => console.log(conteggio++)}>
+          conteggio is {conteggio}
+        </button> */}
+        <button onClick={handleConteggio}>conteggio is {conteggio}</button>
+        <button
+          onClick={function () {
+            handleConteggio2(conteggio, setConteggio);
+          }}
+        >
+          conteggio is {conteggio}
+        </button>
+
+        <button onClick={updateUser}>user is {user.name}</button>
+
+        <button onClick={aggiornaItem}>user is {item}</button>
       </div>
     </>
   );
